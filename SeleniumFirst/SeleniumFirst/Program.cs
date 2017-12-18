c38794736dcccfd70020f813a792986c9ff86ff7
Juan Pablo <jpabadl@unal.edu.co>
2017-12-17 21:51:18 -0500

    Simple code with Selenium C#

---
+++ SeleniumFirst/SeleniumFirst/Program.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //reference for our browser
+            IWebDriver driver = new ChromeDriver();
+
+            //navigate to google page
+            driver.Navigate().GoToUrl("http://www.google.com");
+
+            //Find the Element
+            IWebElement element = driver.FindElement(By.Name("q"));
+
+            //Perform Operation
+            element.SendKeys("executeautomation");
+
+            driver.Close();
+        }
+    }
+}
