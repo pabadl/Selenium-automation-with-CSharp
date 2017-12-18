47f98170555e0484ccd5402961c9b0149c4fb5b1
Juan Pablo <jpabadl@unal.edu.co>
2017-12-17 22:39:43 -0500

    Using NUnit with Selenium C#

--- SeleniumFirst/SeleniumFirst/Program.cs
+++ SeleniumFirst/SeleniumFirst/Program.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
@@ -10,21 +11,54 @@ namespace SeleniumFirst
 {
     class Program
     {
+        //reference for our browser
+        //Global variable
+        IWebDriver driver = new ChromeDriver();
+
         static void Main(string[] args)
         {
-            //reference for our browser
-            IWebDriver driver = new ChromeDriver();
+            
 
+           
+
+           
+            
+        }
+
+
+        [SetUp]
+        public void Initialize()
+        {
             //navigate to google page
             driver.Navigate().GoToUrl("http://www.google.com");
+            Console.WriteLine("Opened URL");
+        }
 
+        [Test]
+        public void ExecuteTest()
+        {
             //Find the Element
             IWebElement element = driver.FindElement(By.Name("q"));
 
             //Perform Operation
             element.SendKeys("executeautomation");
 
+            Console.WriteLine("Executed Test");
+
+
+        }
+
+        [Test]
+        public void NextTest()
+        {
+            Console.WriteLine("Next method");
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
             driver.Close();
+            Console.WriteLine("Close the browser");
         }
     }
 }
